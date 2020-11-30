ef3af649c71c37a1f1112892b2cb9912b176cf7d
15369376182@163.com <test_wss@163.com>
2020-11-30 21:57:52 +0800

    学习

--- dev-01/dev-01.py
+++ dev-01/dev-01.py
@@ -15,7 +15,7 @@
 #     print("hello world")
 #
 # func()
-
+import sys
 import os
 import subprocess
 # r = subprocess.call('cmd /c dir')
@@ -23,7 +23,90 @@ import subprocess
 # # print("没问题",r)
 # # r = os.popen('dir')
 # print("输出结果"+r.decode('GBK'))
-pipe = subprocess.Popen('cmd /c dir',stdout=subprocess.PIPE)
-r = pipe.stdout.read()
-print(r)
-print(r.decode('gbk'))
+# pipe = subprocess.Popen('cmd /c dir',stdout=subprocess.PIPE)
+# r = pipe.stdout.read()
+# print(r)
+# print(r.decode('gbk'))
+
+# args = sys.argv
+
+# def add(a,b):
+#     return a+b
+# if __name__ == "__main__":
+#     if len(args) < 3:
+#         print("arg1,arg2")
+#     else:
+#         r = add(args[1],args[2])
+#         print(r)
+# r = os.environ
+# print(r)
+# print(r["path"],["classpath"])
+
+import threading,time
+# def helloworld():
+#     time.sleep(5)
+#     print("hello %d world" % id)
+#
+#
+# for i in range(5):
+#     t = threading.Thread(target=helloworld,args=(i,))
+#     t.start()
+# print("main thread")
+# def test1():
+#     time.sleep(10)
+#     print("task done " "\n")
+# def test2():
+#     time.sleep(10)
+#     print("task done")
+# lock = threading.Lock()
+# def count(i):
+#     time.sleep(1)
+#     with lock:
+#         print(i)
+# #with  上下文管理器，ender  exit
+# threads = []
+# for i in range(1000):
+#     thread = threading.Thread(target=count,args=(i,))
+#     threads.append(thread)
+#
+# for thread in threads:
+#     thread.start()
+# from urllib import request
+# import threading,queue
+# url = "https://www.baidu.com"
+# q = queue.Queue()
+# for i in range(10):
+#     q.put(i)
+# def get_lvgw():
+#     while True:
+#         try:
+#             q.get(block = False)
+#             r = request.urlopen(url)
+#             print(r.code,"\n","成功")
+#         except queue.Empty:
+#             break
+# ts = []
+# print(ts)
+# for i in range(10):
+#     t = threading.Thread(target=get_lvgw())
+#     ts.append(t)
+#     t.start()
+# if __name__ == "__main__":
+    # start = time.time()
+    # t = threading.Thread(target=test1)
+    # t2 = threading.Thread(target=test2)
+    # t.start()
+    # t2.start()
+    # t.join()
+    # t2.join()
+    # end = time.time()
+    # print(end-start)
+
+def digui(n):
+    if n == 1:
+        return 1
+    else:
+        return n*digui(n-1)
+
+if __name__ == "__main__":
+    print(digui(10))
